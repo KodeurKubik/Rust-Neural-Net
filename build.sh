189ebec85f2dfa2e705f0f84a04803221ac00fd0
Kubik <84036232+KodeurKubik@users.noreply.github.com>
2026-03-05 19:15:25 +0100

    minor changes

--- build.sh
+++ build.sh
@@ -14,6 +14,6 @@ cp ../target/x86_64-pc-windows-gnu/release/demo.exe ../neuralnet-demo/neuralnet-
 upx --best --lzma ../neuralnet-demo/neuralnet-demo.exe
 
 cd ..
-echo "Open a terminal IN THIS FOLDER! And run the executable from the terminal. You can then open http://localhost:3000 and test the model :)" > ./neuralnet-demo/INSTRUCTIONS.txt
+echo "Run the executable corresponding to your system (mac or windows). You can then open http://localhost:3000 and test the model :)" > ./neuralnet-demo/INSTRUCTIONS.txt
 rm -f neuralnet.zip
 zip -r neuralnet.zip ./neuralnet-demo
